add8cf92a02c6e1d502722e73173ad40ffa53dff
penguin-wwy <940375606@qq.com>
2019-12-28 00:23:09 +0800

    add build.rs

---
+++ build.rs
@@ -0,0 +1,16 @@
+extern crate cc;
+
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let java_home = env::var("JAVA_HOME").expect("$JAVA_HOME not found");
+    let include_path = Path::new(&java_home).join("include");
+    let mut build = cc::Build::new();
+    if cfg!(target_os = "linux") {
+        build.include(include_path.join("linux").as_path());
+    } else if cfg!(target_os = "windows") {
+        build.include(include_path.join("win32").as_path());
+    }
+    build.file("src/tools.c").include(include_path.as_path()).compile("libtools.a")
+}
